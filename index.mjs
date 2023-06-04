189725b9a979cec1a294fe0e1af5e7d955241441
fwqaaq <fwqaaq@gmail.com>
2023-06-04 22:16:51 +0800

    split into single file

--- index.mjs
+++ index.mjs
@@ -1,8 +1,11 @@
 import aws from "https://ip-ranges.amazonaws.com/ip-ranges.json" assert {type: "json"}
 import google from "https://www.gstatic.com/ipranges/cloud.json" assert {type: "json"}
 
-function writeAddress(serverIp) {
-  Deno.writeFileSync("serverIp.json", new TextEncoder().encode(JSON.stringify(serverIp, null, 2)))
+function writeAddress(destIP, fileName) {
+  if (fileName.endsWith("json"))
+    Deno.writeFileSync(fileName, new TextEncoder().encode(JSON.stringify(destIP, null, 2)))
+  if (fileName.endsWith("txt"))
+    Deno.writeFileSync(fileName, new TextEncoder().encode(destIP))
 }
 
 async function getAzureJson() {
@@ -40,4 +43,8 @@ azure.forEach(
   }
 )
 
-writeAddress(server)
+// all cloud IP
+writeAddress(server, "all.json")
+
+// single IP
+Object.keys(server).forEach(item => writeAddress(server[item].join('\n'), `${item}.txt`))
